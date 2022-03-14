6ae68567d8744123df997c141fe4cfb3fad8590d
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-14 17:40:57 -0400

    created YourStrategyModal file and updated Card data display

---
+++ src/scenes/YourStrategyModal.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Modal, Button, Card } from 'antd';
+
+export default function YourStrategyModal( { userInput }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Create Strategy
+      </Button>
+      <Modal
+        title="Your strategy:"
+        visible={isModalVisible}
+        onOk={handleOk}
+        okText="Save Strategy"
+        onCancel={handleCancel}
+        cancelText="Edit"
+      >
+        <Card
+          title={userInput.asset}
+          style={{ width: "100%" }}
+          // extra={
+          //   <Button
+          //     type="secondary"
+          //     // icon={<PoweroffOutlined />}
+          //     // loading={loadings[2]}
+          //     // onClick={() => this.enterLoading(2)}
+          //   />
+          // }
+        >
+          <p>Amount: ${userInput.amount}</p>
+          <p>Frequency: {userInput.frequency}</p>
+          <p>Type: {userInput.type}</p>
+          {/* <Button>Edit</Button> */}
+        </Card>
+      </Modal>
+    </>
+  );
+};
+
