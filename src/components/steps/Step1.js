2c0fb25f679c45e769e836a7fcb75ec6d32e54a4
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-15 23:12:32 -0400

    Step1 is saving type selection to types

--- src/components/steps/Step1.js
+++ src/components/steps/Step1.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { Radio } from "antd";
 
-export default function Step1({ types, setTypes }) {
+export default function Step1({ types, setTypes, userInput, setUserInput }) {
   // const [value, setValue] = useState();
 
   const onChange = (e) => {
-    console.log("radio checked", e.target.value);
+    // console.log("radio checked", e.target.value);
     setTypes(e.target.value);
+    setUserInput({ ...userInput, type: e.target.value });
   };
-
-
 
   return (
     <>
-    <h3>I would like to invest in:</h3>
-        <Radio.Group onChange={onChange} value={types}>
-          <Radio value={"Stock"}>Stock</Radio>
-          <Radio value={"ETF"}>ETF</Radio>
-          <Radio value={"Crypto"}>Crypto</Radio>
-        </Radio.Group>
+      <h3>I would like to invest in:</h3>
+      <Radio.Group onChange={onChange} value={types} size="large">
+        <Radio value={"Stock"}>Stock</Radio>
+        <Radio value={"ETF"}>ETF</Radio>
+        <Radio value={"Crypto"}>Crypto</Radio>
+      </Radio.Group>
     </>
   );
 }
