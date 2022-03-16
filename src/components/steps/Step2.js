0943fdb1b1fd0a40ebf4039dcbdd99717becf3fc
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-16 12:20:22 -0400

    created handleChange function to set description

--- src/components/steps/Step2.js
+++ src/components/steps/Step2.js
@@ -1,41 +1,46 @@
 import React from "react";
 import { Cascader } from "antd";
 
+const stocks = [
+  { value: "AAPL", label: "Apple Inc." },
+  { value: "AMZN", label: "Amazon.com, Inc." },
+  { value: "FB", label: "Meta Platforms, Inc." },
+  { value: "GOOGL", label: "Alphabet Inc." },
+  { value: "MSFT", label: "Microsoft Corp." },
+  { value: "NFLX", label: "Netflix, Inc." },
+  { value: "SPOT", label: "Spotify Technology S.A" },
+  { value: "TSLA", label: "Tesla, Inc." },
+];
+
+const etfs = [
+  { value: "SPY", label: "SPDR S&P 500 ETF Trust" },
+  { value: "VTI", label: "Vanguard Total Stock Market ETF" },
+  { value: "QQQ", label: "Invesco QQQ Trust" },
+  { value: "IEFA", label: "iShares Core MSCI EAFE ETF" },
+  { value: "AGG", label: "iShares Core U.S. Aggregate Bond ETF" },
+  { value: "BND", label: "Vanguard Total Bond Market ETF" },
+  { value: "GLD", label: "SPDR Gold Shares" },
+  { value: "ARKK", label: "ARK Innovation ETF" },
+];
+
+const cryptos = [
+  { value: "BTC", label: "Bitcoin" },
+  { value: "ETH", label: "Ethereum" },
+  { value: "XRP", label: "XRP (Ripple)" },
+  { value: "LUNA", label: "Terra" },
+  { value: "ADA", label: "Cardano" },
+  { value: "SOL", label: "Solana" },
+  { value: "DOT", label: "Polkadot" },
+  { value: "DOGE", label: "Dogecoin" },
+  { value: "ATOM", label: "Cosmos" },
+  { value: "ALGO", label: "Algorand" },
+];
+
 export default function Step2({ userInput, setUserInput, types, assets }) {
-  const stocks = [
-    { value: "AAPL", label: "Apple Inc." },
-    { value: "AMZN", label: "Amazon.com, Inc." },
-    { value: "FB", label: "Meta Platforms, Inc." },
-    { value: "GOOGL", label: "Alphabet Inc." },
-    { value: "MSFT", label: "Microsoft Corp." },
-    { value: "NFLX", label: "Netflix, Inc." },
-    { value: "SPOT", label: "Spotify Technology S.A" },
-    { value: "TSLA", label: "Tesla, Inc." },
-  ];
 
-  const etfs = [
-    { value: "SPY", label: "SPDR S&P 500 ETF Trust" },
-    { value: "VTI", label: "Vanguard Total Stock Market ETF" },
-    { value: "QQQ", label: "Invesco QQQ Trust" },
-    { value: "IEFA", label: "iShares Core MSCI EAFE ETF" },
-    { value: "AGG", label: "iShares Core U.S. Aggregate Bond ETF" },
-    { value: "BND", label: "Vanguard Total Bond Market ETF" },
-    { value: "GLD", label: "SPDR Gold Shares" },
-    { value: "ARKK", label: "ARK Innovation ETF" },
-  ];
-
-  const cryptos = [
-    { value: "BTC", label: "Bitcoin" },
-    { value: "ETH", label: "Ethereum" },
-    { value: "XRP", label: "XRP (Ripple)" },
-    { value: "LUNA", label: "Terra" },
-    { value: "ADA", label: "Cardano" },
-    { value: "SOL", label: "Solana" },
-    { value: "DOT", label: "Polkadot" },
-    { value: "DOGE", label: "Dogecoin" },
-    { value: "ATOM", label: "Cosmos" },
-    { value: "ALGO", label: "Algorand" },
-  ];
+  const handleChange = (value, assets) => {
+    setUserInput({ ...userInput, asset: value, description: assets.find(assets => assets.value === value[0]).label })
+  }
 
   if (types === "Stock") {
     assets = stocks;
@@ -52,7 +57,7 @@ export default function Step2({ userInput, setUserInput, types, assets }) {
       placeholder="Take your pick"
       defaultValue={userInput.asset}
       options={assets}
-      onChange={(value) => setUserInput({ ...userInput, asset: value })}
+      onChange={handleChange}
     />
   );
 }
