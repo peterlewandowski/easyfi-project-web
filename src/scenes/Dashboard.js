6820a7b71f11b674e93cae1379f842c5dcec6e21
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-18 14:20:55 -0400

    added conditional rendering on the Dashboard

--- src/scenes/Dashboard.js
+++ src/scenes/Dashboard.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import LoginForm from "../components/common/LoginForm";
 import StrategiesList from "../components/StrategiesList";
+import { UserContext } from "../App";
 
 export default function Dashboard() {
+  const {user, jwt} = useContext(UserContext)
+
   return (
+    <>
+    {!user ? (
+      <div>
+        <LoginForm />
+      </div>
+    ) : (
     <div id="main" style={{ backgroundColor: "#F0FBF0" }}>
       <StrategiesList />;
     </div>
+    )}
+    </>
   );
 }
