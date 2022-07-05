c7539df588b758e00fd663ffe62bd8ba42c45306
Peter Lewandowski <mapiotr2010@gmail.com>
2022-07-04 23:37:48 -0400

    passing currentStrategy props to render the strategy card on Edit Modal

--- src/scenes/EditStrategyCard.js
+++ src/scenes/EditStrategyCard.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Card } from "antd";
-import Item from "antd/lib/list/Item";
 
-export default function EditStrategyCard({ strategy }) {
+export default function EditStrategyCard({ currentStrategy }) {
   return (
     <>
-      <Card title={strategy.asset} style={{ width: "100%" }}>
-        <p>Amount: ${strategy.amount}</p>
-        <p>Frequency: {strategy.frequency}</p>
-        <p>Type: {strategy.type}</p>
-        <p>Description: {strategy.description}</p>
-      </Card>
+      <Card title={currentStrategy.strategy.asset} style={{ width: "100%" }}>
+          <p>Amount: ${currentStrategy.strategy.amount}</p>
+          <p>Frequency: {currentStrategy.strategy.frequency}</p>
+          <p>Type: {currentStrategy.strategy.type}</p>
+          <p>Description: {currentStrategy.strategy.description}</p>
+          <p>Created: {currentStrategy.created}</p>
+          <p>ID: {currentStrategy.id}</p>
+        </Card>
     </>
   );
 }
